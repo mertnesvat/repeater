f3231f0f34e11f9913dfe96a33aad16abffd6afc
mert <mertnesvat@gmail.com>
2020-04-12 02:25:45 +0100

    🎬 init

---
+++ Repeater/Repeater/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Repeater
+//
+//  Created by Mert Neşvat on 4/12/20.
+//  Copyright © 2020 Mert Nesvat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
